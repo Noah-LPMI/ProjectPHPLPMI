678ca79e5e00c36410b1fb517fecbbc222cab0df
Noah_FGC <laplaze.noah@gmail.com>
2025-12-16 15:26:13 +0100

    test listeproduit

---
+++ listeProduit_v.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Liste Produit</title>
+</head>
+
+<body>
+    <div class='flex'>
+        <?php include('menu_c.php') ?>
+        <main>
+            <h1>Liste Produit</h1>
+            <ul>
+                <li>DVD1</li>
+                <li>DVD2</li>
+                <li>DVD3</li>
+                <li>DVD4</li>
+                <li>DVD5</li>
+            </ul>
+        </main>
+    </div>
+</body>
+
+</html>
