fbddc35e7c23df308e4620f92f9ce42ac6d1b34e
isu <182515218+isuu49@users.noreply.github.com>
2026-02-03 17:39:04 +0100

    modif du menu en cours et retrait du bouchon addlien

--- components/menu_v.php
+++ components/menu_v.php
@@ -5,6 +5,20 @@
         **************-->
         <li><a href="/ProjectPHPLPMI/index.php">Tous les produits</a></li>
         <!-- Ajouter menu dynamique sans adminonly (0) -->
+        <?php if(isset($menu)) {
+            while($ligne = $menu->fetch()) {
+                $label = htmlspecialchars($ligne->label, ENT_QUOTES, 'UTF-8');
+                $link = htmlspecialchars($ligne->links, ENT_QUOTES, 'UTF-8');
+                $filter = htmlspecialchars($ligne->filter, ENT_QUOTES, 'UTF-8');
+
+                $url = "/ProjectPHPLPMI/$link";
+                if (!empty($filter)) {
+                    $url .= '?' . $filter;
+                }
+
+                echo "<li><a href='$url'>$label</a></li>";
+            }
+        } ?>
 
         <!-- *************
         Afficher pour tous les utilisateurs non connectés
@@ -29,10 +43,22 @@
             <li><a href="/ProjectPHPLPMI/connected/admin/addCategorie_c.php">Ajouter une  catégorie</a></li>
             <li><a href="/ProjectPHPLPMI/connected/admin/addLien_c.php">Ajouter lien au menu</a></li>
             <!-- Ajouter menu dynamique AVEC adminonly (1) -->
+            <?php if(isset($menuAdmin)) {
+            while($ligne = $menuAdmin->fetch()) {
+                $labelAdmin = htmlspecialchars($ligne->label, ENT_QUOTES, 'UTF-8');
+                $linkAdmin = htmlspecialchars($ligne->links, ENT_QUOTES, 'UTF-8');
+                $filterAdmin = htmlspecialchars($ligne->filter, ENT_QUOTES, 'UTF-8');
+
+                 $url = "/ProjectPHPLPMI/$linkAdmin";
+                if (!empty($filterAdmin)) {
+                    $url .= '?' . $filterAdmin;
+                }
+
+                echo "<li><a href='/ProjectPHPLPMI/$url'>$labelAdmin</a></li>";
+            }
+        } ?>
         <?php endif; ?>
     
     </ul>
 </nav>
-
-<!--listeProduit_c.php?filtre=-->
 
