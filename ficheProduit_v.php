4e12b9235ee09524624f14ed882203099e52e65c
Noah_FGC <laplaze.noah@gmail.com>
2026-02-14 15:19:19 +0100

    update product + liens dans ficheproduit

--- ficheProduit_v.php
+++ ficheProduit_v.php
@@ -27,6 +27,7 @@
             
             <?php if(isset($_SESSION['user_id']) && $_SESSION['user_status']=='admin'){
                 echo "<p><a class='isBtn' href='connected/admin/deleteProduct_c.php?id=".$tableauProduit[0][0]."'>Supprimer</a></p>";
+                echo "<p><a class='isBtn' href='connected/admin/updateProduct_c.php?idprod=".$tableauProduit[0][0]."'>Modifier</a></p>";
             } ?>
             
         </main>
